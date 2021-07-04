1507400d6c597f3e7d16cd61498823aaf8174d49
r616on <r616on@gmail.com>
2021-07-04 21:30:36 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/gh6v1ryjhije6h8y?svg=true)](https://ci.appveyor.com/project/r616on/ahj-homeworks-2-1)
+[![Build status](https://ci.appveyor.com/api/projects/status/ipu2vff8hqhrx2ah?svg=true)](https://ci.appveyor.com/project/r616on/ahj-homeworks-event-1)
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,10 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
