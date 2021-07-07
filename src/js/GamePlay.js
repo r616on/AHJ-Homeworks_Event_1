3638c75e2d83fe0397721dc07e64699c3a167376
r616on <r616on@gmail.com>
2021-07-07 23:40:40 +0300

    done_1_1

--- src/js/GamePlay.js
+++ src/js/GamePlay.js
@@ -10,13 +10,16 @@ export default class GamePlay {
     this.hole = 0;
     this.userHit = 0;
     this.score = 0;
+    this.item = 0;
+    this.timer = 0;
   }
 
   init() {
     this.gameFild.generate(4);
     this.hole = document.querySelectorAll(".hole");
-    // this.eventClicK();
-    this.randomHole();
+    this.eventClicK();
+    this.stepRandom();
+
     // this.eventMouseover();
     // document.addEventListener("DOMContentLoaded", (e) => {
     //   alert("You ready??");
@@ -24,58 +27,45 @@ export default class GamePlay {
     // });
   }
 
-  // eventClicK() {
-  //   this.gameFild.element.addEventListener("click", (e) => {
-  //     if (e.target.classList.contains("goblin")) {
-  //       this.userHit += 1;
-  //     }
-  //   });
-  // }
-  // eventMouseover() {
-  //   this.gameFild.element.addEventListener("mouseover", (e) => {
-  //     if (e.target.classList.contains("goblin")) {
-  //       console.log("111");
-  //       e.target.classList.add("blow");
-  //     }
-  //   });
-  // }
-  randomHole() {
+  eventClicK() {
+    this.gameFild.element.addEventListener("click", (e) => {
+      if (e.target.classList.contains("goblin")) {
+        this.userHit += 1;
+        this.item.remove();
+        clearTimeout(this.timer);
+        this.stepRandom();
+      }
+    });
+  }
+
+  createItem() {
     const item = document.createElement("img");
     item.src = this.goblin.goblinSrc;
     item.classList.add("goblin");
     item.classList.add("blow");
-    item.addEventListener("click", () => {
-      this.userHit += 1;
-    });
-    let index = -1;
-    const interval = setInterval(() => {
-      index = getRandom(0, this.hole.length - 1);
-      this.hole[index].append(item);
-      this.score += 1;
-      if (this.score - this.userHit > 5) {
-        clearTimeout(interval);
-        alert("you lose");
-      }
+    // item.addEventListener("click", () => {
+    //   this.item.remove();
+    //   this.stepRandom();
+    //   this.userHit += 1;
+    // });
+    this.item = item;
+  }
 
-      this.gameScore.innerText = `Score ${this.userHit}`;
-      this.gameFail.innerText = `Fail ${this.score - this.userHit}`;
+  stepRandom() {
+    const index = getRandom(0, this.hole.length - 1);
+    this.createItem();
+    this.hole[index].append(this.item);
+    this.timer = setTimeout(() => {
+      this.item.remove();
+      this.stepRandom();
     }, 1000);
+    this.score += 1;
+    if (this.score - this.userHit > 5) {
+      alert("you lose");
+      this.score = 0;
+      this.userHit = 0;
+    }
+    this.gameScore.innerText = `Score ${this.userHit}`;
+    this.gameFail.innerText = `Fail ${this.score - this.userHit}`;
   }
 }
-
-//  const stop = () => (playing = true),
-//    getHole = (index) => document.getElementById(`hole${index}`),
-//    deactivateHole = (index) => (getHole(index).className = "hole"),
-//    activateHole = (index) => (getHole(index).className = "hole hole_has-mole"),
-//    next = () =>
-//      setTimeout(() => {
-//        if (!playing) {
-//          return;
-//        }
-//        deactivateHole(activeHole);
-//        activeHole = Math.floor(1 + Math.random() * 9);
-//        activateHole(activeHole);
-//        next();
-//      }, 800);
-
-//  next();
